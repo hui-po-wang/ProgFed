346b4ab7b5f0a8cdf801831ddf2bd4c97fb8bd41
Hui-Po Wang <hui.wang@cispa.de>
2022-10-10 13:07:50 +0200

    add settings to mimic centralized learning

---
+++ script-fed/exec_cen.sh
@@ -0,0 +1,6 @@
+#source /huipo/default_env/activate.sh
+cd ..
+
+# basic setting
+python main.py -cfg settings-fed/convnet/cifar10-cen-baseline.yaml
+#python main.py -cfg settings-fed/convnet/cifar10-cen-prog.yaml
