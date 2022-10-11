1d1eb9d83110bc31ad38447288325560fa1d9878
Hui-Po Wang <hui.wang@cispa.de>
2022-10-11 13:07:48 +0200

    update

--- script-fed/exec_cen.sh
+++ script-fed/exec_cen.sh
@@ -2,5 +2,5 @@
 cd ..
 
 # basic setting
-python main.py -cfg settings-fed/convnet/cifar10-cen-baseline.yaml
-#python main.py -cfg settings-fed/convnet/cifar10-cen-prog.yaml
+#python main.py -cfg settings-fed/convnet/cifar10-cen-baseline.yaml
+python main.py -cfg settings-fed/convnet/cifar10-cen-prog.yaml
