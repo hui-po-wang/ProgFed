9945c29bcba5f06b1363d9acd8868b02f1eb92f3
Hui-Po Wang <hui.wang@cispa.de>
2022-10-17 14:34:44 +0200

    update

--- script/exec_prog.sh
+++ script/exec_prog.sh
@@ -1,4 +1,4 @@
-source /p/home/jusers/wang34/juwels/hai_tfda_wp_2_2/huipo/default_env/activate.sh
+#source /p/home/jusers/wang34/juwels/hai_tfda_wp_2_2/huipo/default_env/activate.sh
 cd ..
 
 #python main.py -cfg settings/resnet18/cifar100-prog.yaml
