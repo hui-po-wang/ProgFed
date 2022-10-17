6f2f2ec01f0111dc38510b788e65deb8b367ade9
Hui-Po Wang <a88575847@gmail.com>
2022-10-17 17:05:45 +0200

    Update exec_prog.sh

--- script/exec_prog.sh
+++ script/exec_prog.sh
@@ -1,7 +1,7 @@
 #source /p/home/jusers/wang34/juwels/hai_tfda_wp_2_2/huipo/default_env/activate.sh
 cd ..
 
-#python main.py -cfg settings/resnet18/cifar100-prog.yaml
+python main.py -cfg settings/resnet18/cifar100-prog.yaml
 #python main.py -cfg settings/resnet18/cifar100-prog-restart.yaml
 #python main.py -cfg settings/resnet18/cifar100-prog-restart-10.yaml
 #python main.py -cfg settings/resnet18/cifar100-prog-restart-10-warmup.yaml
@@ -12,4 +12,4 @@ cd ..
 #python main.py -cfg settings/vgg19/cifar100-prog-restart-10-warmup.yaml
 #python main.py -cfg settings/vgg16/cifar100-prog-restart-10-warmup.yaml
 #python main.py -cfg settings/vgg19/cifar100-prog-restart-10.yaml
-python main.py -cfg settings/convnet/cifar10-prog-restart-10-warmup.yaml
+#python main.py -cfg settings/convnet/cifar10-prog-restart-10-warmup.yaml
